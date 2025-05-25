b72185c4a798bc3fbf95c3517ef132db4d2bbb2d
FiReBrO42 <FiReBrO42@users.noreply.github.com>
2025-05-25 12:47:07 +0000

    deploy: 38510cfb50a9c67567463811ea2a65cad79c2232

---
+++ assets/icon-input-checkbox-2.B2FQdF1l.js
@@ -0,0 +1,1 @@
+const n="/RepoOnlineTest/static/icons/input/icon-input-checkbox-active.png",i="/RepoOnlineTest/static/icons/input/icon-input-checkbox-2.png";export{n as _,i as a};
---
+++ assets/img-template-01.dEZeShlC.js
@@ -0,0 +1,1 @@
+const e="/RepoOnlineTest/static/images/template/img-template-01.png";export{e as _};

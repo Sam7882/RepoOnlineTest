e608f3a8b2bbc580b70a17b0ecd936a3628fc557
Sam <sam124674@gmail.com>
2025-06-10 00:52:07 +0800

    [新增] 至頂按鈕

--- src/components/layouts/layout-pcBase.vue
+++ src/components/layouts/layout-pcBase.vue
@@ -12,11 +12,26 @@
 				<slot />
 			</view>
 		</view>
+
+		<!-- 懸浮按鈕 -->
+		<view class="icon-container floating-button" @click="contentScrollTop">
+			<uni-icons class="icons" type="up" size="24" color="var(--text-color-primary)" />
+		</view>
 	</view>
 </template>
 
 <script setup>
 
+// content 滑動頂部
+const contentScrollTop = () => {
+	const content = document.querySelector('.content-area');
+	if (content) {
+		content.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	}
+}
 
 
 </script>
@@ -27,10 +42,23 @@ page {
 }
 
 .pc-layout {
-	--main-area-padding: 64rpx;
+	--main-area-padding: 72rpx;
+
+	@media screen and (min-width: 1280px) {
+		--main-area-padding: 120rpx;
+	}
+
+	@media screen and (min-width: 1440px) {
+		--main-area-padding: 150rpx;
+	}
+
+	@media screen and (min-width: 1920px) {
+		--main-area-padding: 200rpx;
+	}
 }
 
 .pc-layout {
+	position: relative;
 	display: flex;
 	height: 100vh;
 	/* 高度撐滿視窗 */
@@ -53,5 +81,36 @@ page {
 	flex: 1;
 	overflow-y: auto;
 	background-color: #fff;
+
+}
+
+.floating-button {
+	position: fixed;
+	bottom: 5%;
+	right: 50px;
+	z-index: 10;
+	width: 72rpx;
+	height: 72rpx;
+	border-radius: 50%;
+	background-color: var(--text-color-septenary);
+
+	@media screen and (min-width: 1280px) {
+		width: 90rpx;
+		height: 90rpx;
+	}
+
+
+	&:hover {
+		cursor: pointer;
+	}
+
+	&>.icons {
+		color: var(--text-color-secondary) !important;
+		font-size: 40rpx !important;
+
+		@media screen and (min-width: 1280px) {
+			font-size: 48rpx;
+		}
+	}
 }
 </style>

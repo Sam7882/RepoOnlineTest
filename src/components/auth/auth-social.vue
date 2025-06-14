001a6a6d3ac330d04d0917e20a9a989a76233bb4
Sam <sam124674@gmail.com>
2025-06-15 00:11:29 +0800

    [修正] 社群登入按鈕=> 替換 為 SVG

--- src/components/auth/auth-social.vue
+++ src/components/auth/auth-social.vue
@@ -1,10 +1,14 @@
 <template>
+	<!-- TODO: icon使用svg -->
 	<!-- 第三方登入 -->
 	<view class="social-login">
-		<image class="social-icon" src="/static/icons/community/icon-community-facebook.png" />
-		<image class="social-icon" src="/static/icons/community/icon-community-line.png" />
-		<image class="social-icon" src="/static/icons/community/icon-community-google.png" />
-		<image class="social-icon" src="/static/icons/community/icon-community-wechat.png" />
+		<view>
+
+		</view>
+		<image class="social-icon" src="/static/icons/community/icon-community-facebook.svg" />
+		<image class="social-icon" src="/static/icons/community/icon-community-line.svg" />
+		<image class="social-icon" src="/static/icons/community/icon-community-google.svg" />
+		<image class="social-icon" src="/static/icons/community/icon-community-wechat.svg" />
 	</view>
 </template>
 <script>
@@ -14,12 +18,12 @@
 	display: flex;
 	justify-content: center;
 	margin-top: 40rpx;
-	gap: 24rpx;
+	gap: 46rpx;
 }
 
 .social-icon {
-	width: 80rpx;
-	height: 80rpx;
+	width: 100rpx;
+	height: 100rpx;
 
 	&:hover {
 		cursor: pointer;

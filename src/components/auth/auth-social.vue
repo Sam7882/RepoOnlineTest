f7e1ac1cec82f2af7341fde0cc1f847f16233fff
Sam <sam124674@gmail.com>
2025-06-14 23:57:25 +0800

    [新增] [PC] 社群媒體元件組

---
+++ src/components/auth/auth-social.vue
@@ -0,0 +1,28 @@
+<template>
+	<!-- 第三方登入 -->
+	<view class="social-login">
+		<image class="social-icon" src="/static/icons/community/icon-community-facebook.png" />
+		<image class="social-icon" src="/static/icons/community/icon-community-line.png" />
+		<image class="social-icon" src="/static/icons/community/icon-community-google.png" />
+		<image class="social-icon" src="/static/icons/community/icon-community-wechat.png" />
+	</view>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+.social-login {
+	display: flex;
+	justify-content: center;
+	margin-top: 40rpx;
+	gap: 24rpx;
+}
+
+.social-icon {
+	width: 80rpx;
+	height: 80rpx;
+
+	&:hover {
+		cursor: pointer;
+	}
+}
+</style>

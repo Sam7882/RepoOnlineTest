5e9762ac7106cfb14b46b74740f72bceeb7190cd
Sam <sam124674@gmail.com>
2025-06-21 09:33:31 +0800

    [修改][PC] 設定選單 => 基礎版型 與 組件渲染內容

---
+++ src/pages/pc/creator/setMenu-page.vue
@@ -0,0 +1,139 @@
+<template>
+	<view class="set-menu">
+		<view class="header">
+			<view class="header-title">
+				<text>{{ $t('common.setting') }}</text>
+			</view>
+		</view>
+
+		<!-- 內容 -->
+		<view class="content">
+			<!-- 第一層選單 -->
+			<view class="menu-list ">
+				<view class="menu-item" :class="{ 'active': currentPage === 'account' }" @click="handlePage('account')">
+					<text>{{ $t("creator.accountManagement") }}</text>
+					<view class="icon-container">
+					</view>
+				</view>
+				<view class="menu-item" :class="{ 'active': currentPage === 'privacy' }" @click="handlePage('privacy')">
+					<text>{{ $t("creator.privacySetting") }}</text>
+					<view class="icon-container">
+					</view>
+				</view>
+				<view class="menu-item" :class="{ 'active': currentPage === 'message' }" @click="handlePage('message')">
+					<text>{{ $t("creator.messageSetting") }}</text>
+					<view class="icon-container">
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+// TEMP: 創作者設定-帳號設定頁面
+
+import { onShow } from '@dcloudio/uni-app'
+import { checkViewportAutoReplace } from '@/utils/routers';
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+const props = defineProps({
+	page: {
+		type: String,
+		default: 'center'
+	}
+})
+const emit = defineEmits(['switchTab'])
+const currentPage = computed(() => props.page)
+
+const handlePage = (index) => {
+	console.log("🚀 ~ handlePage ~ index:", index)
+	switch (index) {
+		case 'account':
+			emit('switchTab', 'account')
+			break
+		case 'privacy':
+			emit('switchTab', 'privacy')
+			break
+		case 'message':
+			emit('switchTab', 'message')
+			break
+	}
+}
+
+onShow(() => {
+	checkViewportAutoReplace()
+})
+</script>
+
+<style scoped lang="scss">
+.set-menu {
+	--footer-height: 300rpx;
+	position: relative;
+	background: var(--background-color);
+	padding: 70rpx 75rpx;
+	box-shadow: 2rpx 0 8rpx rgba(0, 0, 0, 0.08);
+	display: flex;
+	flex-direction: column;
+	border-right: 4rpx solid var(--text-color-denary);
+	overflow-y: scroll;
+	padding-left: 96rpx;
+	padding-right: 120rpx;
+	padding-top: 88rpx;
+
+	@media screen and (min-width: 961px) and (max-width: 1280px) {
+		padding-left: 72rpx;
+		padding-right: 64rpx;
+		padding-top: 80rpx;
+	}
+}
+
+.header {
+	margin-bottom: 80rpx;
+}
+
+.header-title {
+	font-weight: bold;
+	color: var(--text-color-primary);
+	font-size: var(--font-size-title-pc-xlarge);
+}
+
+
+.content {
+	padding-bottom: var(--footer-height);
+}
+
+.menu-list {
+	display: flex;
+	flex-direction: column;
+	gap: 64rpx;
+}
+
+.menu-item {
+	display: flex;
+	justify-content: space-between;
+	font-size: var(--font-size-title-pc);
+	color: var(--text-color-primary);
+
+	&.active {
+		color: var(--primary-color);
+	}
+
+	&:hover {
+		cursor: pointer;
+	}
+
+	.icon-container {
+		.icon-right {
+			font-size: var(--font-size-content-pc-large) !important;
+			color: var(--text-color-gray3) !important;
+		}
+	}
+}
+
+.status-container {
+	font-size: 28rpx;
+	color: var(--text-color-gray3);
+	margin-right: 24rpx;
+}
+</style>
--- src/pages/pc/creator/setMenu.vue
+++ src/pages/pc/creator/setMenu.vue
@@ -1,216 +1,45 @@
 <template>
-	<view class="set-menu">
-		<c-headerNav :title="menuTitle">
-			<template #left>
-				<view class="header-nav-left-position header-nav-icon-container" @click="prevMenu">
-					<uni-icons class="header-nav-icon" type="left" size="20" color="var(--text-color-primary)"></uni-icons>
-				</view>
-			</template>
-		</c-headerNav>
-
-		<!-- 內容 -->
-		<view class="content">
-			<!-- 第一層選單 -->
-			<view class="menu-list " v-if="menuIndex === '1'">
-				<view class="menu-item" @click="nextMenu('1-1')">
-					<text>{{ $t("creator.accountManagement") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-				<view class="menu-item" @click="nextMenu('1-2')">
-					<text>{{ $t("creator.privacySetting") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-				<view class="menu-item" @click="handleEdit('message')">
-					<text>{{ $t("creator.messageSetting") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-			</view>
-			<!-- 第二層選單 - 帳號管理 -->
-			<view class="menu-list " v-if="menuIndex === '1-1'">
-				<view class="menu-item" @click="handleEdit('phone')">
-					<text>{{ $t("creator.phoneNumber") }}</text>
-					<view class="icon-container">
-						<text>+886*******115</text>
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-				<view class="menu-item" @click="handleEdit('password')">
-					<text>{{ $t("creator.loginPassword") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-				<view class="menu-item" @click="handleEdit('email')">
-					<text>{{ $t("creator.email") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-			</view>
-			<!-- 第二層選單 - 隱私設定 -->
-			<view class="menu-list " v-if="menuIndex === '1-2'">
-				<view class="menu-item" @click="handleEdit('blockade')">
-					<text>{{ $t("creator.blocked") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-				<view class="menu-item" @click="handleEdit('favorite')">
-					<text>{{ $t("creator.myFavorites") }}</text>
-					<view class="icon-container">
-						<uni-icons class="icon-right" type="right" size="16" color="#999" />
-					</view>
-				</view>
-			</view>
-		</view>
-
-		<!-- 底部按鈕 -->
-		<view class="footer-container">
-			<view class="delete-account" v-if="menuIndex === '1-1'">
-				<text>{{ $t("creator.deleteAccount") }}</text>
-			</view>
-			<view class="btn-container">
-				<button type="button" class="btn">
-					{{ $t("creator.switchAccount") }}
-				</button>
-			</view>
-			<view class="btn-container">
-				<button type="button" class="btn">
-					{{ $t("auth.logout") }}
-				</button>
-			</view>
-		</view>
-
-	</view>
+	<layout-pcBase>
+		<template #slideSecondNav>
+			<setMenu-page @switchTab="switchTab" :page="currentPage" />
+		</template>
+		<template #default>
+			<component :is="currentPageComponent" v-if="currentPage" />
+		</template>
+	</layout-pcBase>
 </template>
-
 <script setup>
-// TEMP: 創作者設定-帳號設定頁面
+// TEMP: 創作者設定-帳號設定頁面中介頁
+import setMenuPage from '@/pages/pc/creator/setMenu-page.vue'
+import setMenuAccountPage from '@/pages/pc/creator/setMenu-account-page.vue'
+import setMenuPrivacyPage from '@/pages/pc/creator/setMenu-privacy-page.vue'
+import setMenuMessagePage from '@/pages/pc/creator/setMenu-message-page.vue'
+import { onLoad } from '@dcloudio/uni-app'
 
-import { onShow } from '@dcloudio/uni-app'
-import { router, toCreatorSetMenuEdit, toCreatorBlockadeEdit, checkViewportAutoReplace } from '@/utils/routers';
-import { useI18n } from 'vue-i18n'
-const { t } = useI18n()
-const { back } = router;
-const menuTitle = ref(t('common.setting'))
-const menuIndex = ref('1')
-const nextMenu = (index) => {
-	menuIndex.value = index
-	switch (index) {
-		case '1-1':
-			menuTitle.value = t('creator.accountManagement')
-			break
-		case '1-2':
-			menuTitle.value = t('creator.privacySetting')
+// 透過 tab 來進行內容區塊組件的替換
+const currentPage = ref('')
+const pageComponent = ref({
+	'account': markRaw(setMenuAccountPage),
+	'privacy': markRaw(setMenuPrivacyPage),
+	'message': markRaw(setMenuMessagePage)
+})
+const currentPageComponent = computed(() => {
+	if (!currentPage.value) {
+		return ''
 	}
-}
-const prevMenu = () => {
-	console.log("🚀 ~ prevMenu ~ prevMenu:", prevMenu)
-	if (menuIndex.value === '1') {
-		back()
-		return
-	}
-	menuIndex.value = '1'
-	menuTitle.value = t('common.setting')
-}
-const handleEdit = (index) => {
-	console.log("🚀 ~ handleEdit ~ index:", index)
-	switch (index) {
-		case 'phone':
-			toCreatorSetMenuEdit({
-				type: 'phone'
-			})
-			break
-		case 'password':
-			toCreatorSetMenuEdit({
-				type: 'password'
-			})
-			break
-		case 'email':
-			toCreatorSetMenuEdit({
-				type: 'email'
-			})
-			break
-		case 'blockade':
-			toCreatorBlockadeEdit()
-			break
-		case 'favorite':
-			toCreatorSetMenuEdit({
-				type: 'favorite'
-			})
-			break
-		case 'message':
-			toCreatorSetMenuEdit({
-				type: 'message'
-			})
-			break
-	}
+	console.log("🚀 ~ currentPage:", currentPage)
+	const current = pageComponent.value[currentPage.value]
+	console.log("🚀 ~ currentPageComponent ~ current:", current)
+	return current
+})
+
+const switchTab = (tab) => {
+	console.log("🚀 ~ switchTab ~ tab:", tab)
+	currentPage.value = tab
 }
 
-onShow(() => {
-	checkViewportAutoReplace()
+onLoad((options) => {
+	console.log("🚀 ~ onLoad ~ options:", options)
+	currentPage.value = options.tab
 })
 </script>
-
-<style scoped lang="scss">
-.set-menu {
-	--footer-height: 300rpx;
-}
-
-// 圖標容器
-.header-nav-left-position {
-	position: absolute;
-	left: 48rpx;
-	top: 50%;
-	transform: translateY(-20%);
-	padding-top: 16rpx;
-}
-
-.content {
-	padding-bottom: var(--footer-height);
-}
-
-.menu-item {
-	display: flex;
-	justify-content: space-between;
-	padding: 30rpx 46rpx;
-	border-bottom: 1rpx solid var(--text-color-denary);
-	font-size: 30rpx;
-
-	.icon-container {
-		.icon-right {
-			font-size: 28rpx !important;
-			color: var(--text-color-gray3) !important;
-		}
-	}
-}
-
-// 底部按鈕
-.footer-container {
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	padding: 20rpx 40rpx 60rpx;
-	display: flex;
-	flex-direction: column;
-	gap: 30rpx;
-	background: var(--background-color-light);
-
-	.delete-account {
-		font-size: 28rpx;
-		color: var(--tertiary-color);
-	}
-
-	.btn {
-		padding: 24rpx 0;
-		font-size: 30rpx;
-	}
-}
-</style>

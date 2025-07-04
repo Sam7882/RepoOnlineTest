e518f79cd308fea86257f42e9abc68e5383ee3b9
Sam <sam124674@gmail.com>
2025-07-05 04:52:35 +0800

    [修改][組件] 評論區 RWD 與'窗口

--- src/components/plays/play-popMessage.vue
+++ src/components/plays/play-popMessage.vue
@@ -1,15 +1,17 @@
 <template>
-  <uni-popup ref="popupRef" type="bottom" background-color="#f6f6f6">
-    <view class="popup-container">
+  <uni-popup class="play-popMessage" ref="popupRef" :type="isPc ? 'right' : 'bottom'"
+    :borderRadius="isPc ? '0' : '60rpx 60rpx 0 0'" background-color="#f6f6f6" @change="handleChange"
+    mask-background-color="rgba(0, 0, 0, 0)">
+    <view class="popup-container ">
       <!-- 標題與關閉 -->
       <view class="popup-header">
-        <text class="popup-title">{{ $t('play.comment') }}(88)</text>
+        <text class="popup-title">{{ $t('common.comment') }}(88)</text>
         <uni-icons class="popup-close" type="closeempty" size="16" @click="close" />
       </view>
       <view class="popup-content">
         <scroll-view class="popup-comment-list-scroll" scroll-y>
           <uni-list class="popup-comment-list" :border="false">
-            <template v-for="item in 3" :key="item">
+            <template v-for="(item, index) in 20" :key="item">
               <uni-list-item title="留言" class="popup-comment-list-item" :border="false">
                 <template #body>
                   <!-- 大頭照 -->
@@ -46,13 +48,15 @@
                         <text class="popup-comment-list-item-content-icon-item-text">0</text>
                       </view>
                       <view class="popup-comment-list-item-content-icon-item">
-                        <uni-icons class="icon" type="icon-input-upload" custom-prefix="icon" size="16" />
+                        <uni-icons class="icon" type="icon-input-like" custom-prefix="icon" size="16" />
                         <text class="popup-comment-list-item-content-icon-item-text">1</text>
                       </view>
                     </view>
                     <!-- 查看幾則回覆提示 -->
                     <view class="popup-comment-list-item-content-reply">
-                      <text>{{ $t('common.viewReply', { num: 0 }) }}</text>
+                      <text v-if="index !== 0">{{ $t('common.viewReply', { num: 0 }) }}</text>
+                      <!-- 回復的內容樣子 -->
+                      <play-replyMessage v-else />
                     </view>
                   </view>
 
@@ -61,63 +65,39 @@
             </template>
           </uni-list>
         </scroll-view>
-      </view>
-      <!-- 留言輸入欄位 -->
-      <view class="popup-comment-container">
-        <!-- EMOJI 滑動 -->
-        <view class="popup-comment-emoji">
-          <scroll-view class="popup-comment-emoji-scroll" scroll-x>
-            <text>😀</text>
-            <text>😍</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😟</text>
-            <text>😅</text>
-            <text>😂</text>
-            <text>😀</text>
-            <text>😍</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😟</text>
-            <text>😅</text>
-            <text>😂</text>
-            <text>😀</text>
-            <text>😍</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😟</text>
-            <text>😅</text>
-            <text>😂</text>
-            <text>😀</text>
-            <text>😍</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😟</text>
-            <text>😅</text>
-            <text>😂</text>
-            <text>😀</text>
-            <text>😍</text>
-            <text>😂</text>
-            <text>😲</text>
-            <text>😟</text>
-            <text>😅</text>
-            <text>😂</text>
-          </scroll-view>
-        </view>
-        <!-- 留言輸入框 -->
-        <view class="popup-comment-input">
-          <!-- 頭像 -->
-          <view class="popup-comment-input-avatar avatar-image-container">
-            <image class="popup-comment-input-avatar-image avatar-image"
-              src="/static/images/template/img-template-01.png" mode="widthFix">
-            </image>
+        <!-- 留言輸入欄位 -->
+        <view class="popup-comment-container">
+          <!-- EMOJI 滑動 -->
+          <view class="popup-comment-emoji">
+            <swiper class="popup-comment-emoji-swiper" :indicator-dots="false" :autoplay="false"
+              :display-multiple-items="10" :circular="true">
+              <swiper-item v-for="(emoji, groupIndex) in emojiGroups" :key="groupIndex">
+                <text class="popup-comment-emoji-item" @click="selectEmoji(emoji)">
+                  {{ emoji }}
+                </text>
+              </swiper-item>
+            </swiper>
+
+            <!-- <scroll-view class="popup-comment-emoji-scroll" scroll-x>
+              
+            </scroll-view> -->
           </view>
-          <!-- 輸入框 -->
-          <uni-easyinput type="textarea" class="popup-comment-input-textarea" placeholder="新增評論..." />
+          <!-- 留言輸入框 -->
+          <view class="popup-comment-input">
+            <!-- 頭像 -->
+            <view class="popup-comment-input-avatar avatar-image-container">
+              <image class="popup-comment-input-avatar-image avatar-image"
+                src="/static/images/template/img-template-01.png" mode="widthFix">
+              </image>
+            </view>
+            <!-- 輸入框 -->
+            <input type="textarea" v-model="replayContent" class="popup-comment-input-textarea" placeholder="新增評論..." />
+            <!-- <uni-easyinput type="textarea" v-model="replayContent" class="popup-comment-input-textarea"
+              placeholder="新增評論..." /> -->
+            <view class="icon-container">
+              <uni-icons class="icons" type="icon-common-share" custom-prefix="icon" size="24" @click="sendReplay" />
+            </view>
+          </view>
         </view>
       </view>
     </view>
@@ -126,6 +106,10 @@
 
 <script setup lang="ts">
 // TEMP: 組件-留言彈跳窗口
+import { getElementInfo } from '@/utils/tools';
+import { useViewportStore } from '@/stores/useViewportStore'
+const viewportStore = useViewportStore()
+const { isPc } = storeToRefs(viewportStore)
 const emit = defineEmits(['update:autoDate'])
 const popupRef = ref()
 
@@ -137,23 +121,119 @@ const open = () => {
   popupRef.value?.open()
 }
 
+
+
 defineExpose({ open, close })
+
+const emojiGroups = ref([
+  '😀', '😍', '😂', '😲', '😟', '😅', '😎', '😋', '😴', '😭',
+  '👍', '👎', '👏', '🙌', '👊', '✌️', '👌', '👈', '👉', '👆', '👇', '👌',
+  '❤️', '💛', '💚', '💙', '💜', '💔', '❣️', '💕', '💖', '💗', '💓', '💞',
+  '💯', '⭐', '🌟', '✨', '⚡', '💥', '💢', '💫', '💦', '💨',
+])
+
+const replayContent = ref('')
+const sendReplay = () => {
+  if (!replayContent.value) return
+
+  console.log("🚀 ~ replayContent:", replayContent)
+}
+
+// 選擇 emoji
+const selectEmoji = (emoji: string) => {
+  console.log('選擇的 emoji:', emoji)
+  // 這裡可以將 emoji 插入到輸入框中
+  // 例如：emit('emoji-selected', emoji)
+}
+
+
+
+
+// 獲取底部留言輸入欄位高度
+const updateContainerHeight = () => {
+  console.log("🚀 ~ updateContainerHeight ~ updateContainerHeight")
+  // 使用 uni.createSelectorQuery 獲取元素信息
+  getElementInfo('.popup-comment-container',
+    (info: any) => {
+      if (info && info.height) {
+        updateCSSVariable(info.height)
+        // 設置haveResize為false，阻止窗口未變化時重複執行
+        haveResize.value = false
+      } else {
+        console.log("🚀 ~ updateContainerHeight ~ 元素未找到")
+      }
+    }, getCurrentInstance())
+
+}
+// 將高度設置為 CSS 變數
+const updateCSSVariable = (height: number) => {
+  const dom = document.querySelector('body')
+  if (dom) {
+    (dom as any).style.setProperty('--reply-input-container-height', `${height}px`)
+  }
+}
+
+// 初始化為true，確保第一次開啟時執行
+const haveResize = ref(true)
+
+const handleChange = (e: any) => {
+  if (e.show) {
+    // 延遲執行，確保 DOM 已渲染完成
+    if (!haveResize.value) return
+    nextTick(() => {
+      setTimeout(() => {
+        updateContainerHeight()
+
+      }, 100)
+    })
+  }
+}
+
+
+const setHaveResize = () => {
+  haveResize.value = true
+}
+// 組件掛載後獲取高度
+onMounted(() => {
+  // 監聽視窗大小變化，有變化則設置haveResize為true
+  uni.onWindowResize(setHaveResize)
+})
+
+// 組件卸載時移除監聽
+onUnmounted(() => {
+  uni.offWindowResize(setHaveResize)
+})
 </script>
 
 <style scoped lang="scss">
 ::v-deep(.uni-popup) {
+  & {
+    z-index: var(--z-index-max) !important;
+  }
+}
 
-  // 需同時有圓弧，才能有效果
-  uni-view[name="content"] {
-    border-radius: 32rpx 32rpx 0 0 !important;
-    bottom: 136rpx !important; // 底部導航高
-    max-height: calc(100% - 68px - 25%);
+.popup-container {
+  padding: 32rpx;
+  border-radius: 60rpx 60rpx 0 0;
+  background-color: var(--background-color-light);
+  width: 100%;
+  height: 100%;
+  overflow: visible;
+
+
+
+  @media screen and (min-width: 961px) {
+    border-radius: 32rpx;
+    width: 80vw;
+    max-width: 800rpx;
   }
 
-  .uni-popup__wrapper {
-    background-color: var(--background-color-light) !important;
-    border-radius: 32rpx 32rpx 0 0 !important;
-    height: 100%;
+  @media screen and (min-width: 1920px) {
+    max-width: 1000rpx;
+  }
+
+  @media screen and (min-width: 2560px) {
+    max-width: 1200rpx;
   }
 }
 
@@ -165,19 +245,23 @@ defineExpose({ open, close })
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 12rpx;
+  margin-bottom: 16rpx;
   position: relative;
+
+  @media screen and (min-width: 961px) {
+    margin-bottom: 32rpx;
+  }
 
   .popup-title {
     line-height: 1;
-    font-size: 28rpx;
+    font-size: var(--font-size-content-pc-large);
     font-weight: 500;
     color: var(--text-color-primary);
   }
 
   .popup-close {
     position: absolute;
-    right: 32rpx;
+    right: 0;
     top: 50%;
     transform: translateY(-50%);
   }
@@ -185,6 +269,14 @@ defineExpose({ open, close })
 
 .popup-comment-list-scroll {
   max-height: 570rpx;
+
+  @media screen and (min-width: 768px) and (max-width: 960px) {
+    max-height: 50dvh;
+  }
+
+  @media screen and (min-width: 961px) {
+    max-height: calc(95dvh - var(--reply-input-container-height));
+  }
 }
 
 // 評論區
@@ -197,6 +289,10 @@ defineExpose({ open, close })
     gap: 32rpx;
     padding-bottom: 16rpx;
 
+    @media screen and (min-width: 768px) and (max-width: 960px) {
+      gap: 16rpx;
+    }
+
   }
 
   ::v-deep(.uni-list-item popup-comment-list-item) {
@@ -207,101 +303,98 @@ defineExpose({ open, close })
       display: flex;
       padding: 0;
       gap: 16rpx;
-      padding: 0 32rpx;
-
 
       .uni-list-item__header {
         display: none;
       }
     }
+  }
 
-    // 留言內容資訊
-    .popup-comment-list-item-content {
+  // 留言內容資訊
+  .popup-comment-list-item-content {
+    display: flex;
+    flex-direction: column;
+    gap: 4rpx;
+    flex: 1;
+    padding-right: 12rpx;
+
+    // gap: 8rpx;
+    // 名稱
+    .popup-comment-list-item-content-name {
+      position: relative;
       display: flex;
-      flex-direction: column;
-      gap: 4rpx;
-      flex: 1;
+      align-items: flex-end;
+      gap: 12rpx;
 
-      // gap: 8rpx;
-      // 名稱
-      .popup-comment-list-item-content-name {
-        position: relative;
-        display: flex;
-        align-items: flex-end;
-        gap: 20rpx;
-
-        .popup-comment-list-item-content-name-text {
-          font-size: 28rpx;
-          font-weight: 500;
-        }
-
-        // 創作者
-        .popup-comment-list-item-content-name-tip {
-          font-size: 22rpx;
-          color: var(--primary-color);
-        }
-
-        .popup-comment-list-item-time {
-          display: flex;
-          position: absolute;
-          bottom: 0;
-          right: 0;
-          font-size: 22rpx;
-          color: var(--input-border-color);
-        }
+      .popup-comment-list-item-content-name-text {
+        font-size: var(--font-size-content-pc-large);
+        font-weight: 500;
       }
 
-      // ID
-      .popup-comment-list-item-content-id {
-        display: flex;
-        font-size: 22rpx;
-        line-height: 1;
-        color: var(--input-border-color);
-      }
-
-      // 內文
-      .popup-comment-list-item-content-text-text {
-        margin-top: 4rpx;
-        display: flex;
-        line-height: 1.5;
-        white-space: break-spaces;
-        font-size: 24rpx;
-        color: var(--text-color-primary);
-      }
-
-      .popup-comment-list-item-content-icon {
-        display: flex;
-        gap: 64rpx;
-        margin-top: 8rpx;
-
-
-        .popup-comment-list-item-content-icon-item {
-          display: flex;
-          align-items: center;
-          gap: 12rpx;
-
-          .icon {
-            color: var(--input-border-color) !important;
-            font-size: 28rpx !important;
-          }
-
-          .popup-comment-list-item-content-icon-item-text {
-            font-size: 24rpx;
-            line-height: 1;
-          }
-        }
-      }
-
-      .popup-comment-list-item-content-reply {
-        margin-top: 8rpx;
-        display: flex;
-        line-height: 1.5;
-        font-size: 24rpx;
+      // 創作者
+      .popup-comment-list-item-content-name-tip {
+        font-size: var(--font-size-content-pc-small);
         color: var(--primary-color);
       }
 
+      .popup-comment-list-item-time {
+        display: flex;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        font-size: var(--font-size-content-pc-small);
+        color: var(--input-border-color);
+      }
     }
 
+    // ID
+    .popup-comment-list-item-content-id {
+      display: flex;
+      font-size: var(--font-size-content-pc-small);
+      line-height: 1;
+      color: var(--input-border-color);
+    }
+
+    // 內文
+    .popup-comment-list-item-content-text-text {
+      margin-top: 4rpx;
+      display: flex;
+      line-height: 1.5;
+      white-space: break-spaces;
+      font-size: var(--font-size-content-pc);
+      color: var(--text-color-primary);
+    }
+
+    .popup-comment-list-item-content-icon {
+      display: flex;
+      gap: 48rpx;
+      margin-top: 8rpx;
+
+
+      .popup-comment-list-item-content-icon-item {
+        display: flex;
+        align-items: center;
+        gap: 12rpx;
+
+        .icon {
+          color: var(--input-border-color) !important;
+          font-size: var(--font-size-content-pc-large) !important;
+        }
+
+        .popup-comment-list-item-content-icon-item-text {
+          font-size: var(--font-size-content-pc);
+          line-height: 1;
+        }
+      }
+    }
+
+    .popup-comment-list-item-content-reply {
+      margin-top: 8rpx;
+      display: flex;
+      line-height: 1.5;
+      font-size: var(--font-size-content-pc-small);
+      color: var(--primary-color);
+    }
 
   }
 }
@@ -344,24 +437,30 @@ defineExpose({ open, close })
 // 留言區
 .popup-comment-container {
   border-top: 1px solid var(--text-color-octonary);
-  padding: 8rpx 32rpx;
-  padding-bottom: 36rpx;
+  padding: 8rpx 0;
   display: flex;
   flex-direction: column;
   gap: 16rpx;
+  padding-bottom: var(--iphone-homeBar-height);
 
-  .popup-comment-emoji-scroll {
-    ::v-deep(.uni-scroll-view) {
-      .uni-scroll-view-content {
-        display: flex;
-        white-space: nowrap;
-        gap: 32rpx;
-        font-size: 48rpx;
-      }
+  @media screen and (min-width: 768px) and (max-width: 960px) {
+    padding: 8rpx 0;
+  }
+
+  .popup-comment-emoji-swiper {
+    height: calc(var(--font-size-title-pc-xlarge)*1.7);
+
+    .popup-comment-emoji-item {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: calc(var(--font-size-title-pc-xlarge)*1.2);
     }
   }
 
   .popup-comment-input {
+    position: relative;
     display: flex;
     gap: 24rpx;
     align-items: center;
@@ -372,106 +471,33 @@ defineExpose({ open, close })
     }
 
     .popup-comment-input-textarea {
+      background: var(--text-color-denary);
+      padding: 12rpx 32rpx;
+      border-radius: 100rpx;
       height: 64rpx;
       flex: 1;
+      padding-right: calc(64rpx + 16rpx);
+    }
 
-      ::v-deep(.uni-easyinput) {
+    .icon-container {
+      position: absolute;
+      right: 16rpx;
+      top: 50%;
+      width: 48rpx;
+      height: 48rpx;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transform: translateY(-50%);
+      background: var(--text-color-secondary);
+      border-radius: 100rpx;
+      padding: 8rpx;
 
-        .uni-easyinput__content {
-          height: 100%;
-          border: 0;
-          border-radius: 32rpx;
-          background: var(--text-color-denary) !important;
-
-          .uni-easyinput__content-textarea {
-            height: 100%;
-            min-height: 100%;
-            padding-top: 12rpx;
-
-          }
-        }
-
+      &>.icons {
+        color: var(--text-color-gray3) !important;
+        font-size: 28rpx !important;
       }
     }
-  }
-}
-
-
-.popup-subtitle {
-  font-size: 24rpx;
-  margin-bottom: 80rpx;
-  color: #333;
-}
-
-.example-body {
-  margin-bottom: 48rpx;
-}
-
-.date-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 48rpx;
-  padding: 16rpx;
-  border-radius: 16rpx;
-  gap: 16rpx;
-}
-
-.date-container-item {
-  flex: 1;
-  border-bottom: 1px solid var(--text-color-primary);
-  padding: 0 24rpx;
-  display: flex;
-  flex-direction: column;
-  gap: 16rpx;
-}
-
-.date-value {
-  font-size: 32rpx;
-  font-weight: bold;
-  display: block;
-  text-align: center;
-}
-
-.date-label {
-  font-size: 20rpx;
-  color: #999;
-  display: block;
-  text-align: center;
-}
-
-.result-text {
-  font-size: 24rpx;
-  color: #333;
-  margin-bottom: 48rpx;
-}
-
-.popup-confirm-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.popup-confirm {
-  background-color: #937CFF;
-  color: #fff;
-  border-radius: 28rpx;
-  padding: 24rpx 96rpx;
-  font-size: 28rpx;
-  line-height: 1;
-}
-
-/* 日期選擇器 */
-::v-deep(.uni-calendar-item__weeks-box) {
-  .uni-calendar-item__weeks-box .uni-calendar-item--checked {
-    background-color: var(--primary-color) !important;
-  }
-}
-
-::v-deep(.uni-date-btn--ok) {
-  .uni-datetime-picker--btn {
-    background-color: var(--primary-color) !important;
-    border-radius: 24rpx !important;
   }
 }
 </style>

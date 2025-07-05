d5aeaee503847c83593dd78de72dc85b0934f21c
Sam <sam124674@gmail.com>
2025-07-05 17:01:26 +0800

    [修改] 組件 語系 => 樣式

--- src/components/common/c-langSelector.vue
+++ src/components/common/c-langSelector.vue
@@ -51,6 +51,169 @@ function switchLang(code: string) {
   locale.value = code;
 }
 </script>
+<style scoped lang="scss">
+::v-deep(.uni-popup) {
+  & {
+    z-index: var(--z-index-max) !important;
+    overflow: hidden;
+  }
+}
+
+.popup-container {
+  padding: 32rpx;
+  border-radius: 60rpx 60rpx 0 0;
+  background-color: var(--background-color-light);
+  width: fit-content;
+  overflow: visible;
+
+
+  @media screen and (min-width: 961px) {
+    border-radius: 32rpx;
+    width: 80vw;
+    max-width: 800rpx;
+  }
+
+  @media screen and (min-width: 1920px) {
+    max-width: 1000rpx;
+  }
+
+  @media screen and (min-width: 2560px) {
+    max-width: 1200rpx;
+  }
+}
+
+.popup-header {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  overflow: visible;
+}
+
+.header-title {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    font-size: var(--font-size-title-pc-xlarge);
+  }
+
+  .deco-line {
+    width: 100%;
+    margin: 0;
+    margin-top: 16rpx;
+    height: 4px;
+  }
+}
+
+.popup-close-container {
+  position: absolute;
+  top: 50%;
+  right: 64rpx;
+  z-index: 10;
+  transform: translateY(-70%);
+
+  .popup-close {
+    font-size: var(--font-size-title-pc-xlarge) !important;
+  }
+}
+
+
+.popup-title {
+  font-size: var(--font-size-title-pc-large);
+}
+
+.popup-content {
+  background-color: var(--background-color-light);
+  padding: 40rpx;
+  border-radius: 40rpx;
+  overflow: visible;
+}
+
+.popup-subtitle {
+  font-size: var(--font-size-content-pc-small);
+  margin-bottom: 16rpx;
+  color: var(--text-color-primary);
+}
+
+.popup-options {
+  display: flex;
+  flex-direction: column;
+  gap: 32rpx;
+  margin-bottom: 64rpx;
+}
+
+.popup-option {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.popup-option-main {
+  display: flex;
+  flex-direction: column;
+  gap: 4rpx;
+}
+
+
+.popup-option-title {
+  font-size: var(--font-size-title-pc);
+  color: var(--text-color-primary);
+}
+
+.popup-option-desc {
+  font-size: var(--font-size-content-pc-small);
+  color: var(--text-color-quaternary);
+}
+
+.popup-option-radio-container {
+  position: relative;
+
+  .popup-option-radio {
+    opacity: 0;
+  }
+}
+
+.popup-option-custom-checked {
+  z-index: 10;
+  position: absolute;
+  right: 4px;
+  top: 2px;
+
+  .popup-option-custom-checked-image {
+    width: 34rpx;
+    height: 34rpx !important;
+  }
+
+  ::v-deep(.popup-option-custom-checked-image) {
+
+    // width: 24rpx;
+    // height: 24rpx;
+  }
+}
+
+.popup-confirm-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-confirm {
+  display: inline-block;
+  background-color: var(--primary-color);
+  color: #fff;
+  border-radius: 20rpx;
+  padding: 20rpx 96rpx;
+  font-size: var(--font-size-content-pc);
+  line-height: 1;
+}
+</style>
+
 
 <style lang="scss" scoped>
 .lang-selector {

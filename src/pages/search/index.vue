8dadff980ab8c7f311c3db01292d1e5e7d54f503
Sam <sam124674@gmail.com>
2025-07-11 19:28:39 +0800

    [修正] 組件底部footer => 被景色 & ICON大小

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -8,3 +8,9 @@
 // TEMP: 搜尋首頁中介頁
 import IndexPage from './index-page.vue'
 </script>
+
+<style lang="scss" scoped>
+#app .app-layout {
+	background-color: var(--background-color-dark);
+}
+</style>
